8e66c6946847adb2155637263f5fc1b1406dffea
lizhipeng <popularlzp@163.com>
2022-12-12 16:54:44 +0800

    first push

---
+++ table/src/main/java/com/table/xrecyclerview/ItemTouchHelperAdapter.java
@@ -0,0 +1,14 @@
+package com.table.xrecyclerview;
+
+/**
+ * Created by jianghejie on 16/6/20.
+ */
+
+public interface ItemTouchHelperAdapter {
+
+    void onItemMove(int fromPosition, int toPosition);
+
+
+
+    void onItemDismiss(int position);
+}
